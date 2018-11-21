cccc5864cc056b595dd7d303a717f9a7b0d2f220
mebr0 <aza17e@gmail.com>
2018-11-22 04:47:06 +0600

    reworked Course

--- src/Course.java
+++ src/Course.java
@@ -1,3 +1,4 @@
+import javax.swing.text.ChangedCharSetException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,31 +19,71 @@ public class Course implements ManagingFiles, Serializable {
     }
 
     public String getCourseName() { return courseName; }
+    public void setCourseName(String courseName) { this.courseName = courseName; }
 
     public ArrayList<String> getTeacherLogins() { return teacherLogins; }
-    public void addTeacher(Teacher teacher) { teacherLogins.add(teacher.getLogin()); }
-    public void deleteTeacher(Teacher teacher) { teacherLogins.remove(teacher.getLogin()); }
+    public void addTeacher(String login) { teacherLogins.add(login); }
+    public void deleteTeacher(String login) { teacherLogins.remove(login); }
 
     public ArrayList<String> getStudentLogins() { return studentLogins; }
-    public void addStudent(Student student) { studentLogins.add(student.getLogin()); }
+    public void addStudent(String login) { studentLogins.add(login); }
+    private void deleteStudent(String login) { studentLogins.remove(login); }
+
+    public int getCreditNumber() { return creditNumber; }
+    public void setCreditNumber(int creditNumber) { this.creditNumber = creditNumber; }
+
+    public CourseStatus getStatus(String login) {
+        return statuses.get(login);
+    }
+    public void alterStatus(String login, CourseStatus courseStatus) {
+        if (statuses.get(login) != null) {
+            statuses.replace(login, courseStatus);
+        }
+    }
+
+    public int getScore(String login, int mode) {
+        return scores.get(login)[mode];
+    }
+    public int[] getScores(String login) {
+        return scores.get(login);
+    }
+    public void addScore(String login, int mode, int score) {
+        scores.get(login)[mode] += score;
+    }
 
     @Override
     public ArrayList<CourseFile> getFiles() {
-        return null;
+        return files;
     }
 
     @Override
     public CourseFile getFile(String title) {
-        return null;
+        CourseFile file = null;
+
+        for (CourseFile courseFile: files) {
+            if (courseFile.getTitle().equals(title)) {
+                file = courseFile;
+
+                break;
+            }
+        }
+
+        return file;
     }
 
     @Override
     public void addFile(CourseFile courseFile) {
-
+        files.add(courseFile);
     }
 
     @Override
     public void deleteFile(String title) {
+        for (CourseFile courseFile: files) {
+            if (courseFile.getTitle().equals(title)) {
+                files.remove(courseFile);
 
+                break;
+            }
+        }
     }
 }
